bdf0364b4b159ad504ffd927c6f01d4873d3f6aa
Dave Redmin <david.redmin@beta.dhs.gov>
2018-11-19 12:48:44 -0500

    Increase nmap --host-timeout for portscan job to allow for slower scans to complete

--- cyhy_commander/jobs/portscan.sh
+++ cyhy_commander/jobs/portscan.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nmap -iL *.txt --stats-every 60 -oX - -R -Pn -T4 --host-timeout 120m --max-scan-delay 5ms --max-retries 2 --min-parallelism 32 --defeat-rst-ratelimit -sV -O -sS -p1-65535
+nmap -iL *.txt --stats-every 60 -oX - -R -Pn -T4 --host-timeout 240m --max-scan-delay 5ms --max-retries 2 --min-parallelism 32 --defeat-rst-ratelimit -sV -O -sS -p1-65535
 exitCode=$?
 exit $exitCode
