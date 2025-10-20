a705967cce3be5dec1463fb89067c067b144567e
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-10-20 15:17:34 -0400

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cyhy-commander",
-    version="1.0.0",
+    version="1.0.1",
     author="Mark Feldhousen Jr.",
     author_email="mark.feldhousen@cisa.dhs.gov",
     packages=find_packages(),
