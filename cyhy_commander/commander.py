eed26dca6c1ad524144e70bfc016d74a614bffbc
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-03-23 11:40:00 -0400

    Bump version from 1.1.0 to 1.2.0

--- cyhy_commander/commander.py
+++ cyhy_commander/commander.py
@@ -909,7 +909,7 @@ class Commander(object):
 
 
 def main():
-    args = docopt(__doc__, version="v1.1.0")
+    args = docopt(__doc__, version="v1.2.0")
     workingDir = os.path.join(os.getcwd(), args["<working-dir>"])
     if not os.path.exists(workingDir):
         print >>sys.stderr, 'Working directory "%s" does not exist.  Attempting to create...' % workingDir
